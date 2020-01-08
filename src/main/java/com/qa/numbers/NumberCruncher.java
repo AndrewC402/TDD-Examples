e43a1cd35d96d978a86cf8fb1a733ff08a3b3398
Andrew <andrewc.london2019a@gmail.com>
2020-01-08 11:11:03 +0000

    Find highest number in array test complete

--- src/main/java/com/qa/numbers/NumberCruncher.java
+++ src/main/java/com/qa/numbers/NumberCruncher.java
@@ -1,4 +1,15 @@
 package com.qa.numbers;
 
 public class NumberCruncher {
+
+    public int findHighestNumber(int[] input) {
+        int highestNumber = input[0];
+
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > highestNumber) {
+                highestNumber = input[i];
+            }
+        }
+        return highestNumber;
+    }
 }
--- src/test/java/com/qa/numbers/NumberCruncherTest.java
+++ src/test/java/com/qa/numbers/NumberCruncherTest.java
@@ -1,8 +1,64 @@
+package com.qa.numbers;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberCruncherTest {
 
     @Test
-    void name() {
+    public void find_highest_number_in_array_of_one() {
+        //arrange
+        int input[] = {3};
+        int expectedResult = 3;
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        int actualResult = cut.findHighestNumber(input);
+
+        //arrange
+        assertEquals(expectedResult,actualResult);
     }
+
+    @Test
+    public void find_highest_number_in_array_of_two_where_array_starts_with_largest_number() {
+        //arrange
+        int input[] = {13,4};
+        int expectedResult = 13;
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        int actualResult = cut.findHighestNumber(input);
+
+        //assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void find_highest_number_in_array_of_two_where_array_ends_with_largest_number() {
+        //arrange
+        int input[] = {7,13};
+        int expectedResult = 13;
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        int actualResult = cut.findHighestNumber(input);
+
+        //assert
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void find_highest_number_in_array() {
+        //arrange
+        int input[] = {4,5,-8,3,11,-21,6};
+        int expectedResult = 11;
+        NumberCruncher cut = new NumberCruncher();
+
+        //act
+        int actualResult = cut.findHighestNumber(input);
+
+        //assert
+        assertEquals(expectedResult,actualResult);
+    }
+
 }
